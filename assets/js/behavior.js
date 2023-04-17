ed2b2d5c51dfffba78bc848fc47bd372d9502d3c
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-04-17 15:05:26 +0900

    feature-article, guide main作成

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -11,3 +11,11 @@ let inputTexts = document.querySelectorAll(".topics dd p")
 for (text of inputTexts) {
   text.textContent = truncateText(text, 22)
 }
+
+// 見出し2に付いてる属性『letter-spacing: .5em;』を最後の文字だけ取り去る
+let headingTwo = document.querySelectorAll("h2")
+for (let heading of headingTwo) {
+  let orgText = heading.innerHTML
+  let lastChar = orgText.slice(-1)
+  heading.innerHTML = orgText.replace(lastChar, `<span class="remove-letter-spacing">${lastChar}</span>`)
+}
