5756252d967d72f741e60fb62ffd7ef9b603a6c0
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-05-23 08:50:38 +0900

    光明院　sassの段取りをする

--- 光明院/assets/js/behavior.js
+++ 光明院/assets/js/behavior.js
@@ -1,3 +1,4 @@
+////////////
 // 属性『letter-spacing: .5em;』を最後の文字だけ取り去る
 function killLetterSpace(arr) {
   arr.forEach(elem => {
@@ -11,6 +12,7 @@ const anchor = document.querySelectorAll(".contents-links a")
 killLetterSpace(anchor)
 
 
+////////////
 // <button class="header-menu-button">
 const mainNav = document.getElementById("main-nav")
 const headerMenuButton = mainNav.firstElementChild
@@ -22,7 +24,7 @@ headerMenuButton.addEventListener("click", function() {
   [mainNavMask, contentsLinks].forEach(elem => {
     elem.classList.toggle("pull-up")
   })
-    
+  
   // ハンバーガーメニューを動作させる。
   // この順番を上下に変えるとエラーになる。なぜ？
   Array.from(this.children).forEach(line => {
@@ -30,13 +32,21 @@ headerMenuButton.addEventListener("click", function() {
   })
 })
 
+
+////////////
 // 質問
 // ハンバーガーメニューからリンクをクリックするとハンバーガーメニューが元に戻り該当のリンクへ画面が遷移する仕様をどう書くのか？
 
+
+////////////
+// オープニング
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("main-nav-mask").classList.add("open")
 })
 
+
+////////////
+// スクロール・アニメーション
 const targetElems = document.querySelectorAll(".animation-target")
 const windowHight = window.innerHeight
 
