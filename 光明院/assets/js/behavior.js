d9e066cf3fdf9797df0d4c99192dedc793b01e0a
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-05-25 12:02:41 +0900

    光明院　sp版修正　230523課題完了

--- 光明院/assets/js/behavior.js
+++ 光明院/assets/js/behavior.js
@@ -1,44 +1,28 @@
-////////////
-// 属性『letter-spacing: .5em;』を最後の文字だけ取り去る
-function killLetterSpace(arr) {
-  arr.forEach(elem => {
-    let lastChar = elem.textContent.slice(-1)
-    let preText = elem.textContent.slice(0, -1)
-    elem.innerHTML = `${preText}<span class="remove-letter-spacing">${lastChar}</span>`
-  })
-}
-
-const anchor = document.querySelectorAll(".contents-links a")
-killLetterSpace(anchor)
-
-
-////////////
-// <button class="header-menu-button">
-// const mainNav = document.getElementById("main-nav")
-// const headerMenuButton = mainNav.firstElementChild
-// const mainNavMask = mainNav.lastElementChild
-
-// headerMenuButton.addEventListener("click", function() {
-//   // 記事へのリンクと黒マスクを上げ下げする。
-//   [contentsLinks, mainNavMask].forEach(elem => {
-//     elem.classList.toggle("pull-up")
-//     if (elem.hasAttribute("id")) {
-//       Array.from(elem.children).forEach(li => {
-//         li.addEventListener("click", () => {
-//           elem.classList.remove("pull-up")
-//           mainNavMask.classList.remove("pull-up")
-//         })
-//       })
-//     }
-//   })
-//   // ハンバーガーメニューを動作させる。
-//   // この順番を上下に変えるとエラーになる。なぜ？
-//   Array.from(this.children).forEach(line => {
-//     line.classList.toggle("active")
-//   })
-// })
-
+// behavior page top
+const headerMenuBtn = document.getElementById("header-menu-button")
 const contentsLinks = document.getElementById("contents-links")
+const mainNavMask = document.getElementById("main-nav-mask")
+
+// メニューの切り替え
+headerMenuBtn.addEventListener("click", function () {
+  console.log(headerMenuBtn)
+  this.classList.toggle("active")
+  contentsLinks.classList.toggle("pull-up")
+  mainNavMask.classList.toggle("pull-up")
+})
+
+// ページ内スクロールの際に、メニューを閉じる
+document.querySelectorAll('#contents-links a').forEach(el => {
+  el.addEventListener('click', () => {
+    headerMenuBtn.classList.remove('active')
+    contentsLinks.classList.remove("pull-up")
+    mainNavMask.classList.remove("pull-up")
+  })
+})
+
+
+////////////
+// nav menu
 Array.from(contentsLinks.children).forEach(elem => {
   elem.addEventListener("mouseenter", function() {
     this.classList.add("slide-left")
@@ -47,13 +31,6 @@ Array.from(contentsLinks.children).forEach(elem => {
     this.classList.remove("slide-left")
   })
 })
-// Array.from(contentsLinks.children).forEach(elem, () => {
-//   console.log(elem)
-  //   console.log(this)
-  // })
-// })
-
-
 
 
 ////////////
@@ -73,3 +50,17 @@ document.addEventListener("scroll", function () {
     }
   })
 })
+
+
+////////////
+// 属性『letter-spacing: .5em;』を最後の文字だけ取り去る
+function killLetterSpace(arr) {
+  arr.forEach(elem => {
+    let lastChar = elem.textContent.slice(-1)
+    let preText = elem.textContent.slice(0, -1)
+    elem.innerHTML = `${preText}<span class="remove-letter-spacing">${lastChar}</span>`
+  })
+}
+
+const anchor = document.querySelectorAll(".contents-links a")
+killLetterSpace(anchor)
