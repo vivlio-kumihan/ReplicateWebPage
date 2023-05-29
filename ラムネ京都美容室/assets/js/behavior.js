ec89e1f0fc0ed15c7f721b5bde6a1026f69bef78
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-05-29 10:50:12 +0900

    ラムネ美容室　PC版 => gsap: scroll-trigger設置

--- ラムネ京都美容室/assets/js/behavior.js
+++ ラムネ京都美容室/assets/js/behavior.js
@@ -1,12 +1,52 @@
 ////////////
-// 属性『letter-spacing: .5em;』を最後の文字だけ取り去る
-function killLetterSpace(arr) {
-  arr.forEach(elem => {
-    let lastChar = elem.textContent.slice(-1)
-    let preText = elem.textContent.slice(0, -1)
-    elem.innerHTML = `${preText}<span class="remove-letter-spacing">${lastChar}</span>`
+// gsap
+const scrollTriggers = document.querySelectorAll(".scroll-trigger")
+scrollTriggers.forEach(elem => {
+  gsap.from(elem, .5, {
+    opacity: 0,
+    y: 20,
+    scale: 0.7,
+    ease: "back",
+    scrollTrigger: {
+      trigger: elem,
+      start: "top 80%",
+      end: "50% 70%",
+      markers: true,
+      scrub: .5,
+      // 一度アニメーションしたら終わり
+      // once: true
+    }
   })
-}
+})
 
-const figcaption = document.querySelectorAll("figcaption")
-killLetterSpace(anchor)
+// const scrollTriggers = document.querySelectorAll(".scroll-trigger")
+// scrollTriggers.forEach(elem => {
+//   gsap.from(elem, .5, {
+//     opacity: 0,
+//     y: 20,
+//     scale: 0.7,
+//     ease: "back",
+//     scrollTrigger: {
+//       trigger: elem,
+//       start: "top 80%",
+//       end: "50% 70%",
+//       markers: true,
+//       scrub: .5,
+//       // 一度アニメーションしたら終わり
+//       // once: true
+//     }
+//   })
+// })
+
+// ////////////
+// // 属性『letter-spacing: .5em;』を最後の文字だけ取り去る
+// function killLetterSpace(arr) {
+//   arr.forEach(elem => {
+//     let lastChar = elem.textContent.slice(-1)
+//     let preText = elem.textContent.slice(0, -1)
+//     elem.innerHTML = `${preText}<span class="remove-letter-spacing">${lastChar}</span>`
+//   })
+// }
+
+// const arg = document.querySelectorAll("arg")
+// killLetterSpace(arg)
